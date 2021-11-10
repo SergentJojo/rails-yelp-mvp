757619dab70df6f2a82246e7decf59c728dfdf38
Jonathan Dubois <dubois.bjonathan@gmail.com>
2021-11-10 12:40:46 +0100

    yelp done not clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+p 'start seeding'
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::Address.full_address,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  restaurant.save!
+  5.times do
+    Review.create(
+      content:Faker::Restaurant.review,
+      rating: (1..5).to_a.sample,
+      restaurant_id: restaurant[:id]
+    )
+  end
+  p restaurant.reviews
+end
+
+p Restaurant.all.length
+
+p 'end seeding'
